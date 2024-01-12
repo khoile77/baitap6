a179c9cd253b003ff121474a8100a8b9e451ad77
khoile77 <154046980+khoile77@users.noreply.github.com>
2024-01-12 20:29:15 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+//BÀI 1
+// var  nv1={
+// 	Name : 'Vy',
+// 	Position: 'Thư kí',
+// 	Salary: '20tr/tháng'
+// }
+// var nv2 ={
+// 	Name: 'Hùng',
+// 	Position: 'Quản lí',
+// 	Salary: '17tr/tháng',
+// }
+// var nv3 ={
+// 	Name: 'Quân', 
+// 	Position: 'Bảo vệ',
+// 	Salary: '9tr/tháng',
+
+// }
+// for (var key in   nv1){
+// 	console.log( key+":" + nv1[key])
+// }
+// for (var key in   nv2){
+// 	console.log( key+":" + nv2[key])
+// }
+// for (var key in   nv3){
+// 	console.log( key+":" + nv3[key])}
+//
+//BÀI 2
+var student1 ={
+	Name : 'Khoa',
+	Age : 15,
+	Garde: 10,
+
+
+}
+
+var student2={
+	Name : 'Long',
+	Age : 16,
+	Garde: 11,
+
+
+}
+
+var student3={
+	Name : 'Ngọc',
+	Age : 17,
+	Garde: 12,
+
+
+}
+
+for (var key in student1){
+	console.log(key+":" + student1[key])
+}
+for (var key in student2){
+	console.log(key+ ":" +student2[key])
+}
+for (var key in student3){
+	console.log( key+ ":"+ student3[key])
+}
+//Bài4
+var john ={
+	Firstname:'john',
+	Lastname:'Doe',
+}
+console.log(john.Lastname+","+john.Firstname)
+//bài 3
+var sp1 ={
+	name: "sữa",
+	price:"10k",
+	category:"thực phẩm",
+}
+var sp2 ={
+	name:"xà phòng",
+	price:"26k",
+	category:"đồng dùng cá nhân",
+
+}
+var sp3={
+	name:"bút chì",
+	price:"9k",
+	category:"đồ dùng học tập",
+
+
+}
+for (var key in sp1){
+	console.log(key+":" + sp1[key])
+}
+for (var key in sp2){
+	console.log(key+ ":" +sp2[key])
+}
+for (var key in sp3){
+	console.log( key+ ":"+ sp3[key])
+}
